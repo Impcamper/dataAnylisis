c278a591822cc41f641c6494e253edafd8100fc4
Impcamper <impcamper@gmail.com>
2023-11-24 22:39:53 -0600

    Basics for it in

--- data.py
+++ data.py
@@ -1,1 +1,22 @@
-print("hello")
+#imports
+import requests
+import pandas
+
+#getting database, hypixel doesn't work
+raw=pandas.read_csv("diamonds.csv")
+class sortData:
+    def __init__(self,data):
+        #read data into the class
+        data=data
+    def sortType(self):
+        pass
+    def sortPrice(self):
+        pass
+    
+    
+#print(raw["cut"])
+#options
+item = sortData(raw)
+#orginzation methods
+
+#display methods
